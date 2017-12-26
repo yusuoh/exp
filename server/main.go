be80a4fb30df59b20b082a2e45f1399f268688b9
Yusuo Hu <yusuo@yusuo-macbookpro.roam.corp.google.com>
2017-12-26 10:41:43 -0800

    Add a go project.

---
+++ server/main.go
@@ -0,0 +1,15 @@
+package main
+
+import "fmt"
+import "html"
+import "net/http"
+
+func main() {
+	http.HandleFunc("/hello", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintf(w, "Hello, %q", html.EscapeString(r.URL.Path))
+	})
+	err := http.ListenAndServe(":8080", nil)
+	if err != nil {
+		fmt.Println(err)
+	}
+}
